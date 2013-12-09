37bde2eda46620230f0e5637ca67e358b67d8c1b
apple <apple@applematoiMac-2.local>
2013-12-09 16:27:03 +0800

    Initial Commit

---
+++ bb/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  bb
+//
+//  Created by apple on 13-12-9.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ bb/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  bb
+//
+//  Created by apple on 13-12-9.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
